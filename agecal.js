0dcc3a1c136ae11a4b7729128f349b266323ed5b
pritiyadav888 <68044440+pritiyadav888@users.noreply.github.com>
2021-02-13 13:51:19 -0600

    Add files via upload

---
+++ agecal.js
@@ -0,0 +1,5 @@
+alert("This is just an approximation of your age in days!");
+var age = prompt("What is your age?");
+var year = 365;
+var days = age * year; 
+alert("Your age in days is " + days + " days");
